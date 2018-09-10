11868ff1520a85b6ecb1cacb3de3b2578a740f1f
Mary Jack <maryjackinthebox@gmail.com>
2018-09-10 20:32:26 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,16 +1,14 @@
 class Pirate
-  attr_reader :name, :weight, :height
  
- PIRATES = []
- 
-  def initialize(args)
-    @name = args[:name]
-    @weight = args[:weight]
-    @height = args[:height]
+attr_reader :name, :weight, :height
+   PIRATES = []
+   def initialize(name, weight, height)
+    @name = name
+    @weight = weight
+    @height = height
     PIRATES << self
   end
- 
-  def self.all
+   def self.all
     PIRATES
   end
  
